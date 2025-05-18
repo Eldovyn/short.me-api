f314ee1da99ddf3eccebdb9a79a9c34cd91d05d7
Eldovyn <farras.pramudita@gmail.com>
2025-05-19 00:17:03 +0700

    style(account_active): update style endpoint

--- app/api/account_active.py
+++ app/api/account_active.py
@@ -4,7 +4,7 @@ from ..controllers import AccountActiveController
 account_active_router = Blueprint("account_active_router", __name__)
 
 
-@account_active_router.post("/short.me/account-active")
+@account_active_router.post("/short.me/auth/account-active/request")
 async def send_account_active_email():
     data = request.json
     timestamp = request.timestamp
@@ -12,31 +12,25 @@ async def send_account_active_email():
     return await AccountActiveController.send_account_active_email(email, timestamp)
 
 
-@account_active_router.get("/short.me/account-active/email-verification")
-async def user_account_active_information():
-    data = request.args
+@account_active_router.get("/short.me/auth/account-active/status/<string:token>")
+async def user_account_active_information(token):
     timestamp = request.timestamp
-    token = data.get("token", "")
     return await AccountActiveController.user_account_active_information(
         token, timestamp
     )
 
 
-@account_active_router.patch("/short.me/account-active/verification/email-verification")
-async def user_account_active_verification():
-    json = request.json
+@account_active_router.get("/short.me/account-active/verify/<string:token>")
+async def get_user_account_active_verification(token):
     timestamp = request.timestamp
-    token = json.get("token", "")
-    return await AccountActiveController.user_account_active_verification(
+    return await AccountActiveController.get_user_account_active_verification(
         token, timestamp
     )
 
 
-@account_active_router.get("/short.me/account-active/verification/email-verification")
-async def get_user_account_active_verification():
-    data = request.args
+@account_active_router.patch("/short.me/auth/account-active/active/<string:token>")
+async def user_account_active_verification(token):
     timestamp = request.timestamp
-    token = data.get("token", "")
-    return await AccountActiveController.get_user_account_active_verification(
+    return await AccountActiveController.user_account_active_verification(
         token, timestamp
     )
